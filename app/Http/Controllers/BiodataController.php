a44c15802ecfb284f1198ea186f48511d27008be
achmadar <60347050+achmadar@users.noreply.github.com>
2020-04-14 10:31:29 +0700

    Update BiodataController.php

--- app/Http/Controllers/BiodataController.php
+++ app/Http/Controllers/BiodataController.php
@@ -13,6 +13,11 @@ use DataTables;
 
 class BiodataController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth']);
+    }
+    
     /**
      * Display a listing of the resource.
      *
