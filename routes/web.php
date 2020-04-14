e1043bb380641cbe79ced0281815276cd23bea37
achmadar <60347050+achmadar@users.noreply.github.com>
2020-04-14 11:31:55 +0700

    Update Datatables

--- routes/web.php
+++ routes/web.php
@@ -21,14 +21,6 @@ Route::post("/login", "LoginController@login")->name("login.login");
 
 Route::get("/logout", "LoginController@logout")->name("logout");
 
-// Route::get("/biodata-mahasiswa", "BiodataController@index")->name("biodata.index");
-
-// Route::get("/biodata-mahasiswa/create", "BiodataController@create")->name("biodata.create");
-
-// Route::get("/biodata-mahasiswa/show/{id}", "BiodataController@show")->name("biodata.show");
-
-// Route::get("/biodata-mahasiswa/edit/{id}", "BiodataController@edit")->name("biodata.edit");
-
 Route::resource("biodata", "BiodataController")->middleware("web");
 
 Route::get("/biodata/{biodatum}/delete", "BiodataController@destroy")->name("biodata.destroy");
