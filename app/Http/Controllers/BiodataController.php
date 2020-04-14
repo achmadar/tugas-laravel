6668a26fcc6163056cd4eb2a930f0bc40788ca39
achmadar <60347050+achmadar@users.noreply.github.com>
2020-04-14 11:17:23 +0700

    Update Form Request

--- app/Http/Controllers/BiodataController.php
+++ app/Http/Controllers/BiodataController.php
@@ -93,22 +93,6 @@ class BiodataController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    // public function update(Request $request, $id)
-    // {
-    //     $validation = Validator::make($request->all(), [
-    //         "name" => "string|min:3|max:10|alpha",
-    //         "nim" => "string|min:8",
-    //         "alamat" => "string|min:10",
-    //     ]);
-
-    //     if ($validation->fails()) {
-    //         return redirect()->back()->withErrors($validation)->withInput();
-    //     }
-
-    //     BiodataMahasiswa::where("id", $id)->update($request->except("_token", "_method"));
-    //     return redirect()->route("biodata.index");
-    // }
-
     public function update(UpdateBiodata $request, $id) {
         // $validation = Validator::make($request->all(), [
         //     "name" => "string|min:3|max:10|alpha",
