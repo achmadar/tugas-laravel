7fa97332f381f656c0f9623e604d79ab7aa77376
achmadar <achmadarya@gmail.com>
2020-04-15 09:07:05 +0700

    Export Excel

---
+++ resources/views/biodata/index.blade.php
@@ -0,0 +1,41 @@
+@extends("layout")
+
+@section("content")
+
+    <h1>
+        Daftar Mahasiswa
+        <a href="{{ route('biodata.create') }}" type="button" class="btn btn-primary" style="float: right;">Tambah</a>
+        <a href="{{ route('biodata.export_excel') }}" type="button" class="btn btn-danger mr-1" target="_blank" style="float: right;">Download Excel</a>
+    </h1>
+    
+    <br>
+    <table class="table table-striped table-hover text-center" id="myTable">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>NAMA</th>
+                <th>NIM</th>
+                <th width="20%">ACTION</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse($mahasiswa as $data)
+                <tr>
+                    <td>{{ $data->id }}</td>
+                    <td>{{ $data->name }}</td>
+                    <td>{{ $data->nim }}</td>
+                    <td>
+                        <a href="{{ route('biodata.show', ['biodatum' => $data->id]) }}" class="btn btn-success">Detail</a>
+                        <a href="{{ route('biodata.edit', ['biodatum' => $data->id]) }}" class="btn btn-warning">Edit</a>
+                        <a onclick="return confirm('Apakah Anda yakin?');" href="{{ route('biodata.destroy', ['biodatum' => $data->id]) }}" class="btn btn-danger">Delete</a>
+                    </td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="4">Data belum tersedia!</td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get("/login", "LoginController@showLoginForm")
+    ->name("login");
+Route::post("/login", "LoginController@login")->name("login.login");
+
+Route::get("/logout", "LoginController@logout")->name("logout");
+
+Route::resource("biodata", "BiodataController")->middleware("web");
+
+Route::get("/biodata/{biodatum}/delete", "BiodataController@destroy")->name("biodata.destroy");
+
+Route::get("/export_excel", "BiodataController@export_excel")->name("biodata.export_excel");
